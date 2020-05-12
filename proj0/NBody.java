eef2eecec07fc6ab36e7f657e66380a2f4add24f
juzowa <juzowa72@gmail.com>
2020-05-12 16:32:48 +0800

    create animation and print final result

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -17,6 +17,36 @@ public class NBody {
                 member.draw();
             }
             StdDraw.show();
+
+            StdDraw.enableDoubleBuffering();
+            double time = 0;
+            while(time < T){
+                double [] xForces = new double [planets.length];
+                double [] yForces = new double [planets.length];
+                for(int i = 0; i < planets.length; i++){
+                    xForces[i] = planets[i].calcNetForceExertedByX(planets);
+                    yForces[i] = planets[i].calcNetForceExertedByY(planets);
+                }
+                for (int i = 0; i < planets.length; i++){
+                    planets[i].update(dt, xForces[i], yForces[i]);
+                }
+                StdDraw.clear();
+                StdDraw.picture(0, 0, "images/starfield.jpg");
+                for (Planet member : planets){
+                    member.draw();
+                }
+                StdDraw.show();
+                StdDraw.pause(10);
+                time = time + dt;
+            }
+            //final state
+            StdOut.printf("%d\n", planets.length);
+            StdOut.printf("%.2e\n", radius);
+            for (int i = 0; i < planets.length; i++) {
+                StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+                planets[i].xxPos, planets[i].yyPos, planets[i].xxVel,
+                planets[i].yyVel, planets[i].mass, planets[i].imgFileName);   
+            }
         }	      
     }
 
