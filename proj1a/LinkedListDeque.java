3e68503ecf6761fa2f8beec94d3d92d515a01dd3
juzowa <juzowa72@gmail.com>
2021-03-15 15:38:28 +0800

    modify coding style for gradescope

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,24 +1,24 @@
-public class LinkedListDeque <T> {
+public class LinkedListDeque<T> {
 
     private ListNode sentinel;
     private int size;
 
     private class ListNode {
-        public T item;
-        public ListNode pre;
-        public ListNode next;
+        private T item;
+        private ListNode pre;
+        private ListNode next;
 
 
-        public ListNode(T i, ListNode p, ListNode n) {
+        private ListNode(T i, ListNode p, ListNode n) {
             item = i;
             pre = p;
             next = n;
         }
     }
 
-    public LinkedListDeque(){
+    public LinkedListDeque() {
         size = 0;
-        sentinel = new ListNode(null,null,null);
+        sentinel = new ListNode(null, null, null);
         sentinel.next = sentinel;
         sentinel.pre = sentinel;
     }
@@ -27,32 +27,33 @@ public class LinkedListDeque <T> {
 
     }*/
 
-    public int size(){
+    public int size() {
         return size;
     }
 
-    public void addFirst(T item){
-        sentinel.next = new ListNode(item,sentinel,sentinel.next);
-        sentinel.next.next.pre = sentinel.next;//forget this see else code
+    public void addFirst(T item) {
+        sentinel.next = new ListNode(item, sentinel, sentinel.next);
+        sentinel.next.next.pre = sentinel.next; //forget this see else code
         size = size + 1;
     }
 
-    public void addLast(T item){
-        sentinel.pre = new ListNode(item,sentinel.pre,sentinel);
+    public void addLast(T item) {
+        sentinel.pre = new ListNode(item, sentinel.pre, sentinel);
         sentinel.pre.pre.next = sentinel.pre;
         size = size + 1;
     }
 
-    public boolean isEmpty(){
-        //boolean ans=(size==0)?true:false;//next line code is more elegant
+    public boolean isEmpty() {
+        //boolean ans=(size==0)?true:false;
+        // next line code is more elegant
         return (size == 0);
     }
 
-    public void printDeque(){
+    public void printDeque() {
 
-        if(size != 0){
+        if (size != 0) {
             ListNode temp = sentinel.next;
-            for(int i = 0;i < size;i++) {
+            for (int i = 0; i < size; i++) {
                 System.out.print(temp.item);
                 System.out.print(' ');
                 temp = temp.next;
@@ -61,8 +62,8 @@ public class LinkedListDeque <T> {
         System.out.println();
     }
 
-    public T removeFirst(){
-        if(size!=0) {
+    public T removeFirst() {
+        if (size != 0) {
             ListNode temp = sentinel.next;
             T target = temp.item;
             sentinel.next.next.pre = sentinel;
@@ -74,43 +75,43 @@ public class LinkedListDeque <T> {
         return null;
     }
 
-    public T removeLast(){
-        if(size!=0) {
+    public T removeLast() {
+        if (size != 0) {
             ListNode temp = sentinel.pre;
             T target = temp.item;
             sentinel.pre.pre.next = sentinel;
             sentinel.pre = sentinel.pre.pre;
             temp = null;
-            size = size -1;
+            size = size - 1;
             return target;
         }
         return null;
     }
 
-    public T get(int index){
-        if(index >= size){
+    public T get(int index) {
+        if (index >= size) {
             return null;
         }
         ListNode temp = sentinel.next;
-        for(int i = 0; i < index; i++){
+        for (int i = 0; i < index; i++) {
             temp = temp.next;
         }
         return temp.item;
     }
 
 
-    private T getRecursive(int index , ListNode move){
-        if(index == 0){
+    private T getRecursive(int index, ListNode move) {
+        if (index == 0) {
             return move.item;
         }
-        return getRecursive(index-1,move.next);
+        return getRecursive(index - 1, move.next);
     }
 
-    public T getRecursive(int index){
-        if(index >= size){
+    public T getRecursive(int index) {
+        if (index >= size) {
             return null;
         }
-        return getRecursive(index , sentinel.next);
+        return getRecursive(index, sentinel.next);
     }
 
 
